a5d234f875fc1f344eabd1826a1a4761dd419506
Srivalli <202151189@iiitvadodara.ac.in>
2024-12-03 16:59:49 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,195 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const cookieParser = require("cookie-parser");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const productRouter = require("./routes/Product");
+const categoryRouter = require("./routes/Category");
+const brandRouter = require("./routes/Brand");
+const userRouter = require("./routes/User");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const orderRouter = require("./routes/Order");
+const cors = require("cors");
+const { User } = require("./models/User");
+const { sanitizeUser, isAuth, cookieExtractor } = require("./services/common");
+
+// JWT options
+const opts = {};
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = process.env.SECRET_KEY; // TODO: should not be in code;
+
+app.use(express.static("dist"));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+app.use(passport.authenticate("session"));
+//app.use(express.raw({ type: "application/json" }));
+app.use(express.json());
+app.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+    credentials: true,
+    origin: "http://localhost:5173", // or your frontend URL
+  })
+);
+
+async function main() {
+  await mongoose.connect("mongodb+srv://srivallivangaveti:h3VOwi1IUnLFxM8t@storeease.ixpv9.mongodb.net/?retryWrites=true&w=majority&appName=storeease");
+  console.log("db connected");
+}
+
+main().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+app.use("/products", isAuth(), productRouter.router);
+app.use("/categories", isAuth(), categoryRouter.router);
+app.use("/brands", isAuth(), brandRouter.router);
+app.use("/users", isAuth(), userRouter.router);
+app.use("/auth", authRouter.router);
+app.use("/cart", isAuth(), cartRouter.router);
+app.use("/orders", isAuth(), orderRouter.router);
+
+// Passport Strategies
+passport.use(
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
+    // by default passport uses username
+    //console.log({ email, password });
+    try {
+      const user = await User.findOne({ email: email });
+      //console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: "invalid credentials" }); // for safety
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: "invalid credentials" });
+          }
+          const token = jwt.sign(sanitizeUser(user), process.env.SECRET_KEY);
+          done(null, { id: user.id, role: user.role, token }); // this lines sends to serializer
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    //console.log({ jwt_payload });
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  //console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  //console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+//Payments
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100,
+    currency: "inr",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = process.env.END_POINT_SECRET;
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started at 8080");
+});
